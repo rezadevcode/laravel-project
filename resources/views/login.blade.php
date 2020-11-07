79ad399184a6d686757218aa16b7f84ed2302bfb
rezarepo <reza@mediapuradigitalindonesia.com>
2020-11-07 10:24:27 +0700

    page login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -27,7 +27,7 @@
             </div>
         </div>
     </div>
-    <img class="bg-image" src="../../assets/image/layout.png" alt="bg-image">
+    <img class="bg-image" src="{{ asset('images/layout.png') }}" alt="bg-image">
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
